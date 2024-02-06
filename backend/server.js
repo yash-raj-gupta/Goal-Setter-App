5da060fdde0e6242f51518b51945899f6880b42b
Yash Raj Gupta <yashraj.guptadev@gmail.com>
2024-02-06 22:39:53 +0530

    REST API for goals CRUD done

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const port = process.env.PORT || 5000;
+const colors = require('colors')
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const app = express()
-
+const port = process.env.PORT || 5000;
+connectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
